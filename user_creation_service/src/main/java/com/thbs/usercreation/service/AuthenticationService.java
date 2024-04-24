441fab7dbe32032a06ffb09db67ebc64acb190d3
Gowsickandasamy <gowsickandasamy410@gmail.com>
2024-04-24 09:49:29 +0530

    user creation service commited

--- user_creation_service/src/main/java/com/thbs/usercreation/service/AuthenticationService.java
+++ user_creation_service/src/main/java/com/thbs/usercreation/service/AuthenticationService.java
@@ -1,16 +1,26 @@
 package com.thbs.usercreation.service;
 
 
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,6 +34,7 @@ import com.thbs.usercreation.entity.Token;
 import com.thbs.usercreation.entity.User;
 import com.thbs.usercreation.enumerate.Role;
 import com.thbs.usercreation.enumerate.TokenType;
+import com.thbs.usercreation.exception.UserManagementException;
 import com.thbs.usercreation.repository.TokenRepository;
 import com.thbs.usercreation.repository.UserRepo;
 
@@ -41,7 +52,11 @@ public class AuthenticationService {
     public AuthenticationResponse register(RegisterRequest request) {
         // Check if a user with the given email already exists
         if (repository.findByEmail(request.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("User with the given email already exists");
+            throw new UserManagementException("User with the given email already exists");
+        }
+        
+        if (repository.existsByEmployeeId(request.getEmployeeId())) {
+            throw new UserManagementException("User with employeeID  already exists");
         }
 
         // Create a new user entity based on the registration request
@@ -52,7 +67,7 @@ public class AuthenticationService {
             .lastName(request.getLastname())
             .email(request.getEmail())
             .password(passwordEncoder.encode(request.getPassword()))
-            .isemailverified(false)
+            .isemailverified(true)
             .role(role)
             .businessUnit(request.getBusinessUnit())
             .employeeId(request.getEmployeeId())
@@ -68,53 +83,124 @@ public class AuthenticationService {
     emailService.sendEmail(request.getEmail(),"email verification", verificationUrl);
     System.out.println("-------------------"+verificationUrl);
         // Save the user's token in the repository
-        saveUserToken(savedUser, jwtToken);
+//        saveUserToken(savedUser, jwtToken);
 
         // Return the authentication response containing the token
         return AuthenticationResponse.builder()
-            .accessToken(jwtToken)
+            
             .message("Registration successful but email has to be verified ")
             .build();
     }
-
+    private void executeBatchScript(Integer userId, String jwtToken) {
+        try {
+        // Convert userId to String
+        String userIdStr = String.valueOf(userId);
+       
+               
+     // Read the Python script from the classpath
+        InputStream scriptInputStream = getClass().getClassLoader().getResourceAsStream("scripts/WriteTokenS3.py");
+       
+        if (scriptInputStream == null) {
+            System.err.println("Script file not found in classpath");
+            return;
+        }
+ 
+        // Create a temporary file to write the script content
+        File tempScriptFile = File.createTempFile("WriteTokenS3", ".py");
+       
+        // Write the script content to the temporary file
+        Files.copy(scriptInputStream, tempScriptFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+ 
+        // Create ProcessBuilder
+        ProcessBuilder processBuilder = new ProcessBuilder(
+            "python3",
+            tempScriptFile.getAbsolutePath(), // Path to temporary Python script file
+            userIdStr,  // First argument to the script
+            jwtToken    // Second argument to the script
+        );
+       
+        System.out.println("Executing command: " + String.join(" ", processBuilder.command()));
+       
+        // Start the process
+        Process process = processBuilder.start();
+        System.out.println("Process started");
+       
+        // Read the output and error streams from the batch script
+        BufferedReader reader = new BufferedReader(new
+       InputStreamReader(process.getInputStream()));
+        BufferedReader errorReader = new BufferedReader(new
+       InputStreamReader(process.getErrorStream()));
+        System.out.println("Reading input");
+       
+        // Read output stream
+        String line;
+        while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+        }
+       
+        // Read error stream
+        String errorLine;
+        while ((errorLine = errorReader.readLine()) != null) {
+        System.err.println("Error: " + errorLine); // Print to standard error stream
+        }
+       
+        // Wait for the process to complete
+        int exitCode = process.waitFor();
+        System.out.println("\nExited with error code: " + exitCode);
+       
+        } catch (IOException | InterruptedException e) {
+        e.printStackTrace();
+        }
+        }
     // Method to handle user authentication
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        // Authenticate user using Spring Security's authentication manager
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                request.getEmail(),
-                request.getPassword()
-            )
-        );
-
-        // Retrieve user details from the repository
-        var user = repository.findByEmail(request.getEmail())
-            .orElseThrow();
-
-            String message="";
-        if(user.isIsemailverified()){
-          message="successfully login";
-        }else{
-          message="email has to be verfied";
+        try {
+            // Attempt to authenticate the user
+            authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid email or password.");
         }
-
+   
+        // Retrieve user details from the repository
+        User user = repository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + request.getEmail()));
+   
+        // Check if email is verified
+        String message = user.isIsemailverified() ? "Successfully logged in" : "Email has to be verified";
+   
         // Generate JWT token for the user
         var jwtToken = jwtService.generateToken(user);
-
+   
         // Revoke all existing user tokens
         revokeAllUserTokens(user);
-
+   
         // Save the user's new token in the repository
         saveUserToken(user, jwtToken);
-
+ 
+            // Print user details, user ID, and token
+ 
+            System.out.println("Authenticated User Details:");
+ 
+            System.out.println("User ID: " + user.getId());
+   
+            System.out.println("User Email: " + user.getEmail());
+   
+            System.out.println("Generated Token: " + jwtToken);
+   
+ 
+   
+         // Execute the Python script with user ID and token as arguments
+   
+         executeBatchScript(user.getId(), jwtToken);
+   
         // Return the authentication response containing the token
         return AuthenticationResponse.builder()
             .accessToken(jwtToken)
             .message(message)
             .build();
     }
-
-
     public ResponseEntity<String> verifyEmailToken( String token) {
         System.out.println("+++++++######++++++++"+token);
     if(!jwtService.isTokenExpired(token)){
